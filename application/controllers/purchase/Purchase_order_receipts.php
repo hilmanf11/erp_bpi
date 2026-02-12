6872e1eb7eba4106b94f01cbe6cf612b9ca3874b
Rizki Puji <rizkipuj@gmail.com>
2026-02-12 17:11:36 +0700

    Feature: fix get status temporary Purchase Order Receipts added to Purchase Invoices

--- application/controllers/purchase/Purchase_order_receipts.php
+++ application/controllers/purchase/Purchase_order_receipts.php
@@ -366,7 +366,7 @@ class Purchase_order_receipts extends CI_Controller
                 $this->db->select('a.po_no, a.receipt_no, a.receipt_date, a.awb_no, a.awb_date, a.bc_kind, a.bc_document, a.bc_aju, a.bc_date, b.number as supplier_id, a.lotno, 
                 b.name as supplier_name, a.total_receipt as qty_receipt, a.total_label as qty_label, e.number as category_code, f.division, a.print, g.number as invoice_no');
                 $this->db->select("(CASE 
-                    WHEN g.por_no IS NOT NULL AND LEFT(g.invoice_no, 6) != 'INVTMP' THEN 2 
+                    WHEN g.por_no IS NOT NULL AND LEFT(g.invoice_no, 6) = 'INVTMP' THEN 2 
                     ELSE a.status
                 END) as status", FALSE);
                 $this->db->from('(SELECT *, sum(qty_label) as total_label, sum(qty_receipt) as total_receipt FROM purchase_order_receipts GROUP BY receipt_no ORDER BY status asc) a');
@@ -404,9 +404,9 @@ class Purchase_order_receipts extends CI_Controller
                 }
                 if ($filter_status_invoice !== "") { 
                     if ($filter_status_invoice == 2) {
-                        // status 2 Temporary: Ada di invoice DAN bukan awalan INVTMP
+                        // status 2 Temporary: Ada di invoice DAN dengan awalan INVTMP
                         $this->db->where('g.por_no IS NOT NULL');
-                        $this->db->where("LEFT(g.invoice_no, 6) !=", "INVTMP");
+                        $this->db->where("LEFT(g.invoice_no, 6) =", "INVTMP");
                     } else {
                     $this->db->where('a.status', $filter_status_invoice);
                     }
