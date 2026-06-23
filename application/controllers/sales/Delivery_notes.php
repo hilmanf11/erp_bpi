697b505ff975e4dbe021760ad10861a96c769130
kurniawan <kurniawan130795@gmail.com>
2026-06-23 11:09:22 +0700

    Update Delivery_notes.php

--- application/controllers/sales/Delivery_notes.php
+++ application/controllers/sales/Delivery_notes.php
@@ -980,6 +980,9 @@ class Delivery_notes extends CI_Controller
     //PRINT & EXCEL DATA
     public function print($option = "")
     {
+        ini_set('memory_limit', '-1'); 
+        ini_set('max_execution_time', 0);
+
         if ($option == "excel") {
             $format  = date("Ymd");
             header("Content-type: application/vnd-ms-excel");
